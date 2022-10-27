0a5c5da002d37dfd05215c82db4a1d2ee6f6737f
Josean <joseanmeonez@gmail.com>
2022-10-26 22:39:22 -0600

    Procedimientos Almacenados

--- sopdb_test.sql
+++ sopdb_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2022 a las 05:33:23
+-- Tiempo de generación: 27-10-2022 a las 05:33:59
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `sopdb`
 --
+CREATE DATABASE IF NOT EXISTS `sopdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sopdb`;
 
 DELIMITER $$
 --
